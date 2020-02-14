df13e617b98346efb4bf82a8a970bce4b2cc448c
sidd <siddharthkhanna2k@gmail.com>
2020-02-13 23:33:44 -0500

    figured this out
    took way too long

---
+++ src/java/GUI.java
@@ -0,0 +1,16 @@
+package java;
+
+import javax.swing.*;
+
+public class GUI {
+
+    JFrame frame;
+    JButton button;
+    JPanel panel;
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
